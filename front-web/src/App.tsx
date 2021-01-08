7dfec661675aa30c592fd753a21b522724ca448f
unknown <danielricardosantosgois@gmail.com>
2021-01-07 21:13:38 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+//import Counter from './Counter';
+//import Hello from './Hello';
+
+function App() {
+  return (
+  //  <div>
+  //    <Hello message="Daniel"/>
+  //  </div>
+  //  <div>
+  //    <Counter/>
+  //  </div>
+    <h1>Hello!</h1>
+  );
+}
+
+export default App;
