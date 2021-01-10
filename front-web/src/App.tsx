f3702918b80494d0d142f0a87148d3c0286e0117
unknown <danielricardosantosgois@gmail.com>
2021-01-10 00:27:41 -0300

    Add routes and StepHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
+
 //import Counter from './Counter';
 //import Hello from './Hello';
+
+//Usando rotas do react: nom install react-router-dom
+//instalar os tipos de router-dom pois está usando typescript: npm install @types/react-router-dom
 
 function App() {
   return (
@@ -13,8 +16,7 @@ function App() {
     //    <Counter/>
     //  </div>
     <>
-      <Navbar />
-      <Home />
+      <Routes />
     </>
 
   );
